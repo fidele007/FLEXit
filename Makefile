311c08d2b2b777b4c4c7fb70b5949bf72ff0c07c
fidele007 <forcefidele@gmail.com>
2019-04-21 12:27:09 +0200

    Make it compile for theos on WSL

--- Makefile
+++ Makefile
@@ -1,9 +1,12 @@
+TARGET = iphone:clang:latest:8.0
+
 include $(THEOS)/makefiles/common.mk
 
 TWEAK_NAME = FLEXit
-FLEXit_FILES = FLEXitListener.m Tweak.xm $(wildcard Sources/*.m)
+FLEXit_FILES = FLEXitListener.m Tweak.xm $(wildcard Sources/*.m) $(wildcard Sources/*.mm)
 FLEXit_CFLAGS = -fobjc-arc -w
 FLEXit_LIBRARIES = sqlite3 z activator
+FLEXit_FRAMEWORKS = CoreGraphics UIKit ImageIO QuartzCore
 
 include $(THEOS_MAKE_PATH)/tweak.mk
 
--- setup.sh
+++ setup.sh
@@ -5,5 +5,8 @@ FD=tmpFLEXgit
 git clone https://github.com/Flipboard/FLEX.git "$FD"
 printf "Done. Copying files\n"
 mkdir Sources
-find "$FD"/Classes -type f \( -name "*.h" -o -name "*.m" \) -exec mv {} Sources/ \;
+find "$FD"/Classes -type f \( -name "*.h" -o -name "*.m" -o -name "*.mm" \) -exec mv {} Sources/ \;
+# Fix "ARC forbids synthesizing a property of an Objective-C object with unspecified ownership
+# or storage attribute" (https://github.com/textmate/textmate/pull/1402)
+sed -i 's/(nonatomic) void/(nonatomic, strong) void/g' Sources/*.{m,mm}
 rm -rf "$FD"
